24f64266415612d52fe579f35939fbaa55fbebb0
spartacusrex <patrickcerri@gmail.com>
2021-05-18 15:38:43 +0100

    tokenscale

--- app/src/main/java/org/minima/system/brains/ConsensusNet.java
+++ app/src/main/java/org/minima/system/brains/ConsensusNet.java
@@ -189,8 +189,19 @@ public class ConsensusNet extends ConsensusProcessor {
 			
 			//If there no immediate crossover check backup files..
 			if(cross.isEqual(MiniNumber.MINUSONE)) {
+				//Check if we are below..
+				MiniNumber mytop = getMainDB().getMainTree().getChainTip().getBlockNumber();
+				if(greet.getFirstBlock().isMore(mytop)) {
+					MinimaLogger.log("WE ARE BEHIND THEM - NOTHING TO SEND ");
+					
+					//Set the sync top.. with a little lee way
+					mCurrentSyncTip = greet.getTopBlock().sub(MiniNumber.SIXTYFOUR);
+					
+					return;
+				}
+				
+				//Look for a crossover..
 				PostNetClientMessage(zMessage, new Message(CONSENSUS_NET_GREET_BACKSYNC).addObject("greet", greet));
-//				PostNetClientMessage(zMessage, new Message(CONSENSUS_NET_GREET_BACKSYNC).addObject("greetlist", blocks));
 				return;
 			}
 			
@@ -335,7 +346,6 @@ public class ConsensusNet extends ConsensusProcessor {
 		}else if(zMessage.isMessageType(CONSENSUS_NET_GREET_BACKSYNC)) {
 			//Get the greeting list
 			Greeting greet = (Greeting)zMessage.getObject("greet"); 
-//			ArrayList<HashNumber> blocks = (ArrayList<HashNumber>) zMessage.getObject("greetlist");
 			
 			//Check if we are below..
 			MiniNumber mytop = getMainDB().getMainTree().getChainTip().getBlockNumber();
@@ -345,7 +355,6 @@ public class ConsensusNet extends ConsensusProcessor {
 			}
 			
 			//Check if the cascade is an old block of ours..
-//			HashNumber startblock = blocks.get(0);
 			MiniNumber lowestnum  = greet.getFirstBlock();
 			
 			//Get the Backup manager where OLD blocks are stored..
@@ -426,9 +435,8 @@ public class ConsensusNet extends ConsensusProcessor {
 			MiniNumber casc = getMainDB().getMainTree().getCascadeNode().getBlockNumber();
 			MiniNumber tip  = getMainDB().getMainTree().getChainTip().getBlockNumber();
 			
-			MinimaLogger.log("RESYNC MESSAGE RECEIVED! mycasc:"+casc+" mytip:"+tip);
-			
 			//Drill down 
+			MiniNumber lastblock = MiniNumber.ZERO;
 			ArrayList<SyncPacket> packets = sp.getAllNodes();
 			for(SyncPacket spack : packets) {
 				TxPoW txpow = spack.getTxPOW();
@@ -484,9 +492,28 @@ public class ConsensusNet extends ConsensusProcessor {
 				
 				//Scan for coins..
 				getMainDB().scanMMRSetForCoins(mmr);
+				
+				//What block ios this..
+				lastblock = mmr.getBlockTime();
 			}
 			
 			finishUpSync();
+			
+//			//What is the current diff..
+//			MiniNumber diff = lastblock.sub(tip);
+//			if(diff.isMore(MiniNumber.THOUSAND24)) {
+//				MinimaLogger.log("Clearing Sync Tree");
+//				finishUpSync();
+//				return;
+//			}
+//			
+//			//Are we near the sync tip
+//			if(lastblock.isMoreEqual(mCurrentSyncTip)) {
+//				MinimaLogger.log("SYNC TIP HIT!!");
+//				finishUpSync();
+//			}else {
+//				MinimaLogger.log("RESYNC MESSAGE RECEIVED! mycasc:"+casc+" mytip:"+tip+" lastblock:"+lastblock);
+//			}
 			
 		}else if(zMessage.isMessageType(CONSENSUS_NET_INTRO)) {
 			//Get the Sync Package..
@@ -645,6 +672,10 @@ public class ConsensusNet extends ConsensusProcessor {
 			//DO a basic check..
 			if(!TxPoWChecker.basicTxPowChecks(txpow)) {
 				MinimaLogger.log("ERROR - You've Mined A TxPoW that fails basic checks!");
+				
+				//Remove from mining..
+				getMainDB().remeoveMiningTransaction(txpow.getTransaction());
+				
 				return;
 			}
 			
@@ -660,6 +691,10 @@ public class ConsensusNet extends ConsensusProcessor {
 			
 			if(txpow.getSizeinBytes() > MinimaReader.MAX_TXPOW) {
 				MinimaLogger.log("ERROR - You've Mined A TxPoW that is too BIG! "+txpow.getSizeinBytes()+" / "+MinimaReader.MAX_TXPOW);
+				
+				//Remove from mining..
+				getMainDB().remeoveMiningTransaction(txpow.getTransaction());
+				
 				return;
 			}
 			
